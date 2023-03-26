771273650e11b1a2157be8e0ef68a1bb01b3ba0c
Pablo <pablito@me.com>
2023-03-27 00:06:54 +0100

    message test files

---
+++ lib/env.rb
@@ -0,0 +1,5 @@
+export PATH=$PATH:/Users/pablo/projects/makers/golden-square/golden-takeaway/lib
+
+export TWI_AC_SID=0088
+export TWI_AUTH_TOKEN=1199
+
---
+++ lib/test.rb
@@ -0,0 +1,10 @@
+require "twilio-ruby"
+require "date"
+# require "dotenv"
+
+twi_ac_sid = ENV["TWI_AC_SID"]
+
+puts "Hello"
+puts twi_ac_sid
+
+puts "key = #{twi_ac_sid}"
