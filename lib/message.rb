0c16342f93cf6ea35bbbec687b2b925132d13ad9
Pablo <pablito@me.com>
2023-03-28 19:26:26 +0100

    fix for mobile number format

--- lib/message.rb
+++ lib/message.rb
@@ -5,7 +5,7 @@ class Message
   def initialize(mobile = "02345123456")
     @sid = ENV['TWILIO_AC_SID']
     @auth = ENV['TWILIO_AUTH_TOKEN']
-    @receiver = mobile
+    @receiver = mobile[1..-1].prepend("+44")
     @sender = ENV['TWILIO_NUMBER'] # twilio number
     @time = Time.now
     @eta = @time + 2400
@@ -13,10 +13,6 @@ class Message
 
   def message_body
     "Thank you! Your order was placed and will be delivered before #{@eta.strftime("%k:%M")}"
-  end
-
-  def mobile_edit(mobile)
-    @receiver = 1 # complete this to transform the number to a valid number
   end
 
   def send_text
